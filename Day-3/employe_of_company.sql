9f7a1f2666ce28bb61b995317896575501b93bbc
bannurikushal <bannurikushal59@gmail.com>
2026-07-23 06:41:49 +0530

    Day-3 files uploaded

---
+++ Day-3/employe_of_company.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MYSQL.employe_of_company(
+    Emp_ID TEXT,
+    Name VARCHAR(200),
+    Gender TEXT,
+    Department TEXT,
+    salary FLOAT,
+    Start_Date DATE,
+    Employment_Type TEXT,
+    Work_Location TEXT
+);
